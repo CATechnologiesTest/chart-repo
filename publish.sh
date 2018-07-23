8c44436d71a418925856775cc64ea847c61172e6
limscoder <dthomp325@gmail.com>
2018-07-23 16:15:05 -0600

    Updated chart.

--- publish.sh
+++ publish.sh
@@ -4,9 +4,8 @@ cd ./incubator
 
 CHART_PATH=../../agent/helm/freshtracks-agent
 
-helm package $CHART_PATH
-helm repo index .
-
-git add .
-git commit -am "Updated chart."
+helm package $CHART_PATH && \
+helm repo index . && \
+git add ../ && \
+git commit -m "Updated chart." && \
 git push origin HEAD:master
